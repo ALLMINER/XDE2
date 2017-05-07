434bdb3d330364c36cf35f2719a090e64405ca7d
Kees de Wit <keesdewit@keesdewit.nl>
2017-05-07 16:47:05 +0200

    Rename example address

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -43,7 +43,7 @@ SendCoinsDialog::SendCoinsDialog(QWidget *parent) :
 
 #if QT_VERSION >= 0x040700
     /* Do not move this to the XML file, Qt before 4.7 will choke on it */
-    ui->lineEditCoinControlChange->setPlaceholderText(tr("Enter a XDE2 address (e.g. afdVPH9xuGgnpWkuy1JLeF9XhqrjJmq1W6)"));
+    ui->lineEditCoinControlChange->setPlaceholderText(tr("Enter a XDE2 address (e.g. XERHRBcBXtPLMPToYaB8MyC2DBzVQQ6pJq)"));
 #endif
 
     addEntry();
--- src/qt/sendmessagesdialog.cpp
+++ src/qt/sendmessagesdialog.cpp
@@ -34,7 +34,7 @@ SendMessagesDialog::SendMessagesDialog(Mode mode, Type type, QWidget *parent) :
 #if QT_VERSION >= 0x040700
      /* Do not move this to the XML file, Qt before 4.7 will choke on it */
     if(mode == SendMessagesDialog::Encrypted)
-        ui->addressFrom->setPlaceholderText(tr("Enter a XDE2 address (e.g. afdVPH9xuGgnpWkuy1JLeF9XhqrjJmq1W6)"));
+        ui->addressFrom->setPlaceholderText(tr("Enter a XDE2 address (e.g. XERHRBcBXtPLMPToYaB8MyC2DBzVQQ6pJq)"));
  #endif
     addEntry();
 
