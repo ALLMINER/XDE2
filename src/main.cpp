e56cea0ecc304d264afe778c162693e595a88100
Kees de Wit <keesdewit@keesdewit.nl>
2017-05-22 20:06:52 +0200

    Fix stake reward

--- src/main.cpp
+++ src/main.cpp
@@ -1493,6 +1493,9 @@ int64_t GetProofOfStakeReward(const CBlockIndex* pindexPrev, int64_t nCoinAge, i
     int64_t nRewardCoinYear = IsCommunityWallet(destination) ? COMMUNITY_WALLET_MAX_MINT_PROOF_OF_STAKE : MAX_MINT_PROOF_OF_STAKE;
     
     int64_t nSubsidy = nCoinAge * nRewardCoinYear / 365 / COIN;
+	
+	if (pindexBest->nHeight >= 5250)
+		nSubsidy = nCoinAge * nRewardCoinYear / 365 / 7;
 
     return nSubsidy + nFees;
 }
